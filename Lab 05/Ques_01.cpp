f5c359536c6d5cb37564405870c146eed86fc682
vashistha <vashistha.chaturvedi410@gmail.com>
2024-02-12 00:42:44 +0530

    lab5

---
+++ Lab 05/Ques_01.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 100;
+int n, m, x[MAXN][MAXN];
+
+
+int main() {
+
+    freopen("input1.txt","r",stdin);
+    freopen("output1.txt","w",stdout);
+
+    int t; cin>>t;
+    while(t--){
+        scanf("%d%d", &n, &m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d", &x[i][j]);
+            }
+        }
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (x[i][j] == 0) continue;
+                int cnt = 1;
+                queue<vector<int> > q;
+                q.push({i, j});
+                x[i][j] = 0;
+                while (!q.empty()) {
+                    vector<int> front = q.front();
+                    q.pop();
+                    for (int dx = -1; dx <= 1; dx++) {
+                        for (int dy = -1; dy <= 1; dy++) {
+                            int nx = dx + front[0];
+                            int ny = dy + front[1];
+                            if (nx >= 0 && ny >= 0 && nx < n && ny < m && x[nx][ny] == 1) {
+                                x[nx][ny] = 0;
+                                cnt++;
+                                q.push({nx, ny});
+                            }
+                        }
+                    }
+                }
+                maxi = max(maxi, cnt);
+            }
+        }
+        printf("%d\n", maxi);
+    }
+    return 0;
+}
